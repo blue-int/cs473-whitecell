1b1a6c820ad3903ae1d603e2ff8c50440fb8652c
블루 <chlwjdtn1120@gmail.com>
2020-11-16 23:36:24 +0900

    Changed few chat logic

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="chat-box">
+  <div>
     ChatBox!
-    <div v-for="chat in chatList" :key="chat.id">
-      {{ chat.displayName }}: {{ chat.msg }}
+    <div class="chat-box">
+      <div v-for="chat in chatList" :key="chat.id">
+        {{ chat.displayName }}: {{ chat.msg }}
+      </div>
     </div>
-    <input v-model="text" placeholder="chat" />
+    <input v-model="text" placeholder="chat" @keyup.enter="send()" />
     <button @click="send()">send</button>
   </div>
 </template>
 
 <script>
-import { db } from '@/components/firebaseInit'
 import firebase from 'firebase/app'
 export default {
   name: 'ChatBox',
@@ -21,7 +22,7 @@ export default {
       currentUser: firebase.auth().currentUser
     }
   },
-  created() {
+  /*  created() {
     db.collection('lobby')
       .doc(this.$route.params.id)
       .collection('chatList')
@@ -30,23 +31,27 @@ export default {
           return { id: doc.id, ...doc.data() }
         })
       })
-    /*
-       test code
+    // test code
     for (let i = 0; i < 100; i++) {
       this.chatList.push({
         id: i,
         displayName: 'BLUE',
         msg: 'hiiiiiiiiiii'
       })
-    }*/
+    }
+  }, */
+  updated() {
+    const chatBox = this.$el.querySelector('.chat-box')
+    chatBox.scrollTop = chatBox.scrollHeight
   },
   methods: {
     send() {
+      if (this.text.length == 0) return
       this.chatList.push({
         uid: this.currentUser.uid,
         displayName: this.currentUser.displayName,
         photoURL: this.currentUser.photoURL,
-        timeCreated: firebase.firestore.FieldValue.serverTimestamp(),
+        timeCreated: firebase.firestore.Timestamp.now(),
         msg: this.text,
         likes: 0
       })
@@ -61,6 +66,6 @@ export default {
   width: 500px;
   height: 100px;
   border: 1px solid black;
-  overflow: scroll;
+  overflow-y: scroll;
 }
 </style>
