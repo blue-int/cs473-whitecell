d9f155c364c522c069ca4ffb1656ca8fdb13f931
Jungsoo Choi <chlwjdtn1120@gmail.com>
2020-11-16 15:24:42 +0900

    Stop Stream added

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -4,6 +4,7 @@
     <div>Room {{ $route.params.id }}</div>
     <div>{{ title }}</div>
     <div>{{ master }}</div>
+    <button @click="stopStream()">Stop stream</button>
     <vue-plyr>
       <div class="plyr__video-embed">
         <iframe
@@ -43,6 +44,14 @@ export default {
     } else {
       this.title = doc.data().title
       this.master = doc.data().master
+    }
+  },
+  methods: {
+    stopStream() {
+      db.collection('lobby')
+        .doc(this.$route.params.id)
+        .delete()
+      this.$router.push('/lobby')
     }
   }
 }
